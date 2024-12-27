7642afa2c7068d2c34698961a482ef1fc2a9284f
RBaykan <dmental123@gmail.com>
2024-12-27 13:47:43 +0300

    Initial commit

---
+++ src/main/java/com/spring/web/dto/CreateUser.java
@@ -0,0 +1,31 @@
+package com.spring.web.dto;
+
+
+import com.spring.web.validation.EmailValid;
+import com.spring.web.validation.PasswordMatches;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@PasswordMatches
+public class CreateUser{
+
+	
+	@NotBlank String firstname;
+	@NotBlank private String lastname;
+	@NotBlank private String username;
+	@NotBlank private String password;
+	@NotBlank private String repassword ;
+	
+	@NotBlank @EmailValid private String email;
+
+}
